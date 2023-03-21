75ce2191a1ae92f0ba72154f27de3b6dbbaa2d4c
GPSPic <picard.guillaume47@gmail.com>
2023-03-21 17:20:33 +0000

    MVP complete

--- console.py
+++ console.py
@@ -2,3 +2,23 @@ from models.artist import Artist
 from models.album import Album
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
+
+artist_repository.delete_all()
+album_repository.delete_all()
+
+artist1 = Artist("Madonna")
+artist_repository.create(artist1)
+artist2 = Artist("Prince")
+artist_repository.create(artist2)
+
+album1 = Album("The Immaculate Collection", "Pop", artist1)
+album_repository.create(album1)
+album2 = Album("Purple Rain", "Pop", artist2)
+album_repository.create(album2)
+
+print(artist_repository.select(1))
+print(album_repository.select(1))
+
+print(artist_repository.select_all())
+print(album_repository.select_all())
+
--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -5,3 +5,33 @@ from models.artist import Artist
 
 import repositories.artist_repository as artist_repository
 
+def create(album):
+    sql = "INSERT INTO albums (title, genre, artist_id) VALUES (%s, %s, %s) RETURNING *"
+    values = [album.title, album.genre, album.artist.id]
+    results = run_sql(sql, values)
+    id = results[0]['id']
+    album.id = id
+    return album
+
+def delete_all():
+    sql = "DELETE FROM albums"
+    run_sql(sql)
+
+def select(id):
+    album = None
+    sql = "SELECT * FROM albums WHERE id = %s"
+    values =[id]
+    results = run_sql(sql, values)
+    if results:
+        result = results[0]
+        album = Album(result['title'], result['genre'], result['artist_id'], result['id'])
+    return album
+
+def select_all():
+    albums = []
+    sql = "SELECT * FROM albums"
+    results = run_sql(sql)
+    for row in results:
+        album = Album(row['title'], row['genre'], row['artist_id'], row['id'])
+        albums.append(album)
+    return albums
