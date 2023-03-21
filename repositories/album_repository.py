40922eec3c4e041ba9fcc15983d2c7f3f0601cb1
GPSPic <picard.guillaume47@gmail.com>
2023-03-21 16:16:41 +0000

    create all files and base classes

--- repositories/album_repository.py
+++ repositories/album_repository.py
@@ -1,4 +1,7 @@
-from models.artist import Artist
+from db.run_sql import run_sql
+
 from models.album import Album
+from models.artist import Artist
+
 import repositories.artist_repository as artist_repository
-import repositories.album_repository as album_repository
+
